6b3037bc710dfd11098004e3a6b5b351d1d757cf
Monty Rhodes <montyrhodes8@gmail.com>
2022-01-18 18:31:42 +0000

    Added user input to change the path of input, need to add automation to check if there is a forward slash on the end of the input string. If yes, only concatenate with "*"

--- regextest.pl
+++ regextest.pl
@@ -1,8 +1,20 @@
 #!/usr/bin/perl
+#Author: Monty Rhodes
+#Created on: 10/01/2022
+#The purpose of this script is to sort files from one directory to two separate directories, depending on the filename of said files.
 
+use strict;
+use warnings;
 use v5.10;
 
-@array = ( "fwfkmowmfs01e05fAEGR.MP4", "FEF1080pilm1", "GEhtebS03e30eb", "Yes");
+print "Please input the path of directory to be sorted e.g. /home/user/Downloads/ : ";
+my $name = <STDIN>;
+chomp $name;
+
+my $fullpath = "${name}*";
+say $fullpath;
+
+my @array = ( "fwfkmowmfs01e05fAEGR.MP4", "FEF1080pilm1", "GEhtebS03e30eb", "Yes");
 
 my @dirs = glob('/var/lib/transmission-daemon/downloads/*');
 
