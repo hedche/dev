2a383d7d3459f66702303bf2b08f1c5662b5d857
hedche <montyrhodes8@gmail.com>
2022-03-27 18:39:31 +0100

    WIP, but a start

---
+++ cpp/hello.world/cout.cpp
@@ -0,0 +1,7 @@
+#include "cout.h"
+#include <iostream>
+
+void cout(const char* message)
+{
+    std::cout << message << std::endl;
+}
---
+++ cpp/hello.world/main.cpp
@@ -0,0 +1,9 @@
+#include "cout.h"
+#include <iostream>
+
+int main(){
+    int x = 5;
+    if (x ==5)
+        cout("Hello World!");
+
+}
