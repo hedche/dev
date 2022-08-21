2f7767faaab161fadcbcce237eb48636fb6b1612
Monty <montyrhodes.apple@gmail.com>
2022-08-21 18:57:08 +0100

    anagramOrNot working

---
+++ python/anagramOrNot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# This script will take two strings as an anagram and print out wether or not they are anagrams.
+
+# First we make both strings lower case to standardise them
+str1 = sorted(str(input("Please enter the first string:\n")).lower())
+str2 = sorted(str(input("Please enter the second string:\n")).lower())
+
+if(len(str1) == len(str2)):
+    if(str1 == str2):
+        print("Strings provided ARE anagrams!")
+        quit()
+
+print("Strings provided are NOT anagrams!")
--- python/q2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-# This script will take two strings as an anagram and print out wether or not they are anagrams.
-
-read string1
-read string2
-
-
